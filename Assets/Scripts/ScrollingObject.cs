0ae469aec5783885ac6165c6a03fc0516f554135
xp7353 <80015324+xp7353@users.noreply.github.com>
2021-04-06 11:48:15 +0900

    RungameUpdate

---
+++ Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour
+{
+    public float speed = 10f;
+    
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+}
