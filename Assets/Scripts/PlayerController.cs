e23880cc7ee4e48ca56391971040c2eb9171a0fc
xp7353 <80015324+xp7353@users.noreply.github.com>
2021-04-13 12:03:08 +0900

    updata0413

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -55,6 +55,8 @@ public class PlayerController : MonoBehaviour
         playerAudio.Play();
         playerRigidbody.velocity = Vector2.zero;
         isDead = true;
+
+        GameManager.instance.OnPlayerDead();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
--- Assets/Scripts/ScrollingObject.cs
+++ Assets/Scripts/ScrollingObject.cs
@@ -8,6 +8,9 @@ public class ScrollingObject : MonoBehaviour
     
     void Update()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if(!GameManager.instance.isGameover)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
     }
 }
